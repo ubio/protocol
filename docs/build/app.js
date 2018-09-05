f7d7d1bf9cccdb7fb225d5690e24ac50a02b8300
randunel <dkrandu@yahoo.com>
2018-09-05 13:42:32 +0300

    Added confirmation output.

--- docs/build/app.js
+++ docs/build/app.js
@@ -38798,6 +38798,9 @@ module.exports={
         "estimatedSpeed": {
             "typeRef": "#/domains/BroadbandSignup/types/Anything"
         },
+        "confirmation": {
+            "typeRef": "#/domains/BroadbandSignup/types/Anything"
+        },
         "finalPrice": {
             "description": "Emitted immediately before placing order, when final price is available.<br/>Automation will request <code>finalPriceConsent</code> input which should match this object.",
             "typeRef": "#/domains/Generic/types/PriceConsent"
