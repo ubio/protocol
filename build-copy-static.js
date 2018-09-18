910699eeccf897d258de5975a48351796bea3b07
Mark S. Everitt <mark.s.everitt@gmail.com>
2018-09-18 14:46:50 +0100

    Updates scripts and dependencies.

---
+++ build-copy-static.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { join } = require('path');
+const { promises: { copyFile, readdir, mkdir } } = require('fs');
+
+async function lazyMkdir(path) {
+    try {
+        await mkdir(path);
+    } catch (e) {
+        if (e.code !== 'EEXIST') {
+            throw e;
+        }
+    }
+}
+
+async function copyFiles(src, dest) {
+    await lazyMkdir(dest);
+
+    // TODO remove withTypes when 10.10.1 or above is released.
+    const dirents = await readdir(src, { withFileTypes: true, withTypes: true });
+
+    await dirents.map(async dirent => {
+        if (dirent.isFile()) {
+            return await copyFile(join(src, dirent.name), join(dest, dirent.name));
+        }
+
+        if (dirent.isDirectory()) {
+            const nextSrc = join(src, dirent.name);
+            const nextDest = join(dest, dirent.name);
+
+            return await copyFiles(nextSrc, nextDest);
+        }
+    });
+}
+
+copyFiles(join(__dirname, 'docs'), join(__dirname, 'public'))
+    .catch(error => console.error(error)); // eslint-disable-line no-console
--- build-html-cache.js
+++ build-html-cache.js
@@ -1,13 +1,11 @@
-#!/usr/bin/env node
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 
-const indexHtmlFile = path.join(__dirname, 'docs', 'index.html');
+const indexHtmlFile = path.join(__dirname, 'public', 'index.html');
 
 const text = fs.readFileSync(indexHtmlFile, 'utf-8');
-const patched = text
-    .replace(/"\/build\/app\.js\?.*?"/g, `"/build/app.js?${Date.now()}"`);
+const patched = text.replace('?cache-buster', `?${Date.now()}`);
 
-fs.writeFileSync(indexHtmlFile, patched, 'utf-8');
+fs.writeFileSync(indexHtmlFile, patched, 'utf8');
