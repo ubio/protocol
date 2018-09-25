c23e7bd954c23c17361ca72f2963e8bc775e448a
Boris Okunskiy <boris@okunskiy.name>
2018-09-25 17:25:25 +0300

    Prune exports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 'use strict';
 
-const schema = require('./schema');
 const Protocol = require('./protocol');
 const ProtocolProvider = require('./provider');
 const Domain = require('./domain');
 
-const protocol = new Protocol(schema);
-
 module.exports = {
-    schema,
-    protocol,
     Protocol,
     ProtocolProvider,
     Domain,
